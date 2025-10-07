cebaa049c87db12a8cbe44c278bc2451bf8cc336
Trevor Marr <>
2025-10-07 15:49:00 -0700

    Fix dependencies and add required upgrades.js file

---
+++ upgrades.js
@@ -0,0 +1,13 @@
+module.exports = [
+	/*
+	 * Place your upgrade scripts here
+	 * Remember that once it has been added it cannot be removed!
+	 */
+	// function (context, props) {
+	// return {
+	// README.mdupdatedConfig: null,
+	// README.mdupdatedActions: [],
+	// README.mdupdatedFeedbacks: [],
+	// }
+	// },
+]
